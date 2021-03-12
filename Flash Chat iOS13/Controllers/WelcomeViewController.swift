3c0ea0642b1a58e0ceadc594dcbecceda170f880
Vladislav Koshelev <VOKoshelyev@sberbank.ru>
2021-03-12 17:20:29 +0300

    add cocoapods && pod 'CLTyping'

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,31 +7,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        flashChatAnimating()
-    }
-    
-    // MARK: - Flash Chat Animating
-    
-    private func flashChatAnimating() {
         
-        let titleText = "⚡️FlashChat"
-        var charIndex = 0.0
-        
-        titleLabel.text = ""
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
