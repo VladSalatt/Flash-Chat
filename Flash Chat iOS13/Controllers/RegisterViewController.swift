03a92afd8228cc39da628a052a02a073cbe3b81b
Vladislav Koshelev <VOKoshelyev@sberbank.ru>
2021-03-14 20:44:06 +0300

    add register and signIn for users

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        errorLabel.text = ""
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let eror = error {
+                    print(eror.localizedDescription)
+                    self.errorLabel.text = eror.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
