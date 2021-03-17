a4a7e87e5f3577acb475cf97ca9e1431758d7c67
Vladislav Koshelev <VOKoshelyev@sberbank.ru>
2021-03-17 20:38:46 +0300

    add cell xib

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "first@dot.com", body: "hey"),
+        Message(sender: "second@dot.com", body: "fine")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        // Регестрируем ячейку, созданную через xib
+    
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -29,8 +39,24 @@ class ChatViewController: UIViewController {
             try Auth.auth().signOut()
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
+            print("Error signing out: %@", signOutError)
         }
     }
+}
+
+// Этот протокол отвечает за заполнение таблиц данными
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+    
     
 }
